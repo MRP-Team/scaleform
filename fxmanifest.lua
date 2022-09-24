93bba42931607022e14440228bde29483707b9df
abdel1touimi <abdel1.touimi@gmail.com>
2022-09-24 17:09:31 +0100

    min scaleform

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'MRP Team'
+
+description 'scaleform'
+version '1.0.0'
+
+client_scripts {
+  'scaleform.lua'
+}
+
+lua54 'yes'
--- scaleforms/client.lua
+++
@@ -1,8 +0,0 @@
-Citizen.CreateThread(function()
-    local s = Scaleform.Request("mp_big_message_freemode")
-    s:CallFunction("SHOW_SHARD_WASTED_MP_MESSAGE", "SOME TEXT", "SOME MORE TEXT", 5)
-    while true do
-        Citizen.Wait(0)
-        s:Draw2D()
-    end
-end)
